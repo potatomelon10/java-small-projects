27e8068f2b142f5e86ccb6ed83927eea5078100f
Test_Zero <64817663+potatomelon10@users.noreply.github.com>
2020-09-12 00:00:26 -0400

    Create WeirdAlgo.java

---
+++ CSES/Introductory/WeirdAlgo.java
@@ -0,0 +1,20 @@
+public class WeirdAlgo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long number = scan.nextLong();
+        System.out.print(number + " ");
+        while(number != 1){
+            if(number%2 == 0){
+                number /= 2;
+
+            }
+            else{
+                number = number * 3 + 1;
+            }
+            System.out.print(number + " ");
+        }
+    }
+    public static void bruh(){
+        System.out.println("Bruh");
+    }
+}
